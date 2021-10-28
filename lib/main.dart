1c8122eb8e2d446b4b8a2a55173c8db5c56e2776
Choi-Ju12g <choi-ju12g@naver.com>
2021-10-28 19:13:19 +0900

    add appbar's raised button and centerTitle true

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,16 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text('practice'),
+        title: Text('Haksang Doctor'),
+        centerTitle: true,
+        elevation: 0.0,
+        leading: RaisedButton(
+          child:Text('LogIn'),
+          color: Colors.orange,
+          onPressed: (){
+            
+            },
+        ),
       ),
       body: Container(
         color: Colors.green,
