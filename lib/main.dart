3d835522a236525fe3580159228fc51f04445304
Choi-Ju12g <choi-ju12g@naver.com>
2021-11-04 20:04:34 +0900

    debug error of grammar

--- lib/main.dart
+++ lib/main.dart
@@ -107,48 +107,6 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
-//
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-//
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//
-//         ),
-//
-//       ),
-//
-//
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
 
 final Map<int, Color> _whiteMap = {
   50 : Color(0xFFFFFFFF),
