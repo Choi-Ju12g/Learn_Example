1e339a7b4acdb0d835ba47f8bc3cd268c439d6e3
Choi-Ju12g <choi-ju12g@naver.com>
2021-12-08 16:39:41 +0900

    object

--- momentum/app.js
+++ momentum/app.js
@@ -1,2 +1,15 @@
-console.log(433);
-console.log("fe")
+let a = 5;
+let b = 2;
+const myNam = "ju12g"
+const player = {
+    name: "ju12g",
+    age: 23,
+    fat: true,
+    phone: "010-6612-8891"
+}
+
+console.log(a+3);
+console.log(a+"fe")
+
+console.log(player);
+console.log(player.name);
