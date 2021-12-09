c6b40fcfcc3142854122e1834d0185a44c8738b1
Choi-Ju12g <choi-ju12g@naver.com>
2021-12-09 09:41:58 +0900

    create object funtion, In JS function is also property

--- momentum/app.js
+++ momentum/app.js
@@ -1,15 +1,15 @@
 let a = 5;
 let b = 2;
-const myNam = "ju12g"
+const myNam = "ju12g";
 const player = {
     name: "ju12g",
     age: 23,
     fat: true,
-    phone: "010-6612-8891"
-}
+    phone: "010-6612-8891",
+    sayText: function(str) {
+        console.log(str);
+    }
+};
 
-console.log(a+3);
-console.log(a+"fe")
-
-console.log(player);
-console.log(player.name);
+sayText(player.name);
+sayText(player.age);
