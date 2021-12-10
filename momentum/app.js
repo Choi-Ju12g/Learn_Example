9ed4374b917936fd9172ac5aa415c44b37c803b9
Choi-Ju12g <choi-ju12g@naver.com>
2021-12-10 14:11:42 +0900

    simple calculator using js

--- momentum/app.js
+++ momentum/app.js
@@ -1,15 +1,33 @@
-let a = 5;
-let b = 2;
-const myNam = "ju12g";
-const player = {
-    name: "ju12g",
-    age: 23,
-    fat: true,
-    phone: "010-6612-8891",
-    sayText: function(str) {
-        console.log(str);
+const cal = {
+    add : function(a,b){
+        console.log(a+b);
+    },
+    minus : function(a,b){
+        console.log(a-b);
+    },
+    dot : function(a,b){
+        console.log(a*b);
+    },
+    divide : function(a,b){
+        console.log(a/b);
+    },
+
+    square: function(a,b){
+        console.log(a**b);
+    },
+    abs : function(a){
+        if (a > 0) {
+            console.log(a);
+        }else{
+            console.log(a*(-1));
+        }
     }
 };
 
-sayText(player.name);
-sayText(player.age);
+cal.add(3,2);
+cal.minus(3,2);
+cal.dot(3,2);
+cal.divide(3,2);
+cal.square(3,2);
+cal.abs(2);
+cal.abs(-4);
