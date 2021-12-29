74bd910cde97165de07ae02d635ab1167a9db948
Choi-Ju12g <choi-ju12g@naver.com>
2021-12-29 14:43:55 +0900

    make todo remove function

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,11 +2,22 @@ const toDoForm = document.querySelector("#todo-form");
 const toDolist = document.querySelector("#todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
+function deleteTodo(){
+    const target = this.parentElement;
+    target.remove();
+}
+
 function panitToDo(newToDo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
+    const button = document.createElement("button");
     span.innerText = newToDo;
+    button.innerText = "❌";
+
+    button.addEventListener("click",deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
+
     toDolist.appendChild(li);
 }
 
