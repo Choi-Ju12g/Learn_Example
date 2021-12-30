94ba6efe3335e747985b4c8ae22ed720ce99c4a4
Choi-Ju12g <choi-ju12g@naver.com>
2021-12-30 20:10:05 +0900

    local storage

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,8 +2,17 @@ const toDoForm = document.querySelector("#todo-form");
 const toDolist = document.querySelector("#todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
+const TODOS_KEY = "toods";
+
+let todos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY,JSON.stringify(todos));
+}
+
 function deleteTodo(){
     const target = this.parentElement;
+    //const target = event.target.parentElement; 로 해도 위와 동일 -> event 파라미터 필요
     target.remove();
 }
 
@@ -25,7 +34,16 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    todos.push(newTodo);
     panitToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
+
+const getSavedToDos = localStorage.getItem(TODOS_KEY);
+if(saveToDos !== null){
+    const parsedToDos = JSON.parse(getSavedToDos);
+    todos = parsedToDos;
+    parsedToDos.forEach(panitToDo);
+}
