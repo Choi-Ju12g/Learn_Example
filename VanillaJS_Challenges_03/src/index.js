189366315cda47fcd67460bbdf4664b38429694f
Choi-Ju12g <choi-ju12g@naver.com>
2022-01-17 20:42:02 +0900

    ver3 final

--- VanillaJS_Challenges_03/src/index.js
+++ VanillaJS_Challenges_03/src/index.js
@@ -1,34 +1,21 @@
 const range = document.querySelector(".range-form");
 const userInput = document.querySelector(".input-form");
-const body = document.querySelector("body");
 const playBtn = document.querySelector(".play-button");
 const result = document.querySelector(".result");
 
 function onClick(e) {
-    e.preventDefault();
-    let randomNumber = Math.floor(Math.random()*(parseInt(range.value)+1));
-    let output;
-    if(userInput.value == randomNumber){
-        output = "You won!!"
-        result.innerText = 
-        `You chose: ${userInput.value}, the machine chose: ${randomNumber}.
+  e.preventDefault();
+  let randomNumber = Math.floor(Math.random() * (parseInt(range.value) + 1));
+  let output;
+  if (parseInt(userInput.value) === randomNumber) {
+    output = "You won!!";
+    result.innerText = `You chose: ${userInput.value}, the machine chose: ${randomNumber}.
         ${output}`;
-    }else{
-        output = "You lost!!"
-        result.innerText = 
-        `You chose: ${userInput.value}, the machine chose: ${randomNumber}.
+  } else {
+    output = "You lost!!";
+    result.innerText = `You chose: ${userInput.value}, the machine chose: ${randomNumber}.
         ${output}`;
-    }
-    console.log(typeof(range.value));
-    console.log(result.innerText)
-}
-
-function numkeyCheck(e){
-    if(e.key >= 0 && e.key<= 9){
-        return true;
-    }else{
-        return false;
-    }
+  }
 }
 
 playBtn.addEventListener("click", onClick);
