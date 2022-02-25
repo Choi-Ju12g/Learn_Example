325058630d7edeaac40fb8ebd8b7c64afaa53a40
Choi-Ju12g <choi-ju12g@naver.com>
2022-02-25 15:36:36 +0900

    change design

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -1,10 +1,47 @@
 const images = ["0.png","1.png","2.png"];
+const colors = [
+    "#ef5777",
+    "#575fcf",
+    "#4bcffa",
+    "#34e7e4",
+    "#0be881",
+    "#f53b57",
+    "#3c40c6",
+    "#0fbcf9",
+    "#00d8d6",
+    "#05c46b",
+    "#ffc048",
+    "#ffdd59",
+    "#ff5e57",
+    "#d2dae2",
+    "#485460",
+    "#ffa801",
+    "#ffd32a",
+    "#ff3f34"
+  ];
 
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
 const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+//document.body.appendChild(bgImage);
 
-console.log(bgImage);
+const button = document.querySelector("#changeBgBtn");
+const body = document.querySelector("body");
+function changeBackgroundColor(){
+  let randomNumIndex = []
+  for(i = 0 ; i<2; i++){
+    const randomNum = Math.floor(Math.random()*colors.length);
+    if(randomNumIndex.indexOf(randomNum) === -1){
+      randomNumIndex.push(randomNum)
+    }else{
+      i--;
+    }
+  }
+  body.style.background = `linear-gradient(to right, ${colors[randomNumIndex[0]]},${colors[randomNumIndex[1]]})`;
+
+}
+
+changeBackgroundColor();
+button.addEventListener("click",changeBackgroundColor);
