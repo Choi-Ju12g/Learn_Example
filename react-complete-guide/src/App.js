e3f9b08b4e0c92fcbc19c59ea854cbf0c47fe3be
Choi-Ju12g <choi-ju12g@naver.com>
2022-03-27 13:19:46 +0900

    udemy lecture learn setting

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+function App() {
+  return (
+    <div className="App">
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
