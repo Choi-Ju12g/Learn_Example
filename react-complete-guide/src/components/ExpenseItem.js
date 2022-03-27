71b6fcee70c74fc74f5dd014a78933f347b95787
Choi-Ju12g <choi-ju12g@naver.com>
2022-03-27 14:00:48 +0900

    make props

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,15 +1,19 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    const expenseDate = new Date(2022, 2, 27);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 290.58;
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("ko", { month: "long" });
+  const day = props.date.toLocaleString("ko", { day: "2-digit" });
+  const years = props.date.getFullYear();
   return (
-    <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
-      <div className='expense-item__description'> 
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+    <div className="expense-item">
+      <div>
+        <div>{years}</div>
+        <div>{month}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
